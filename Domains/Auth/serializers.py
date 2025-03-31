6ebce832f8e8bef9891fafbd09471700111e0637
ZahraFalih <zahra@schemaplus.io>
2025-03-31 15:53:47 +0300

    .

--- Domains/Auth/serializers.py
+++ Domains/Auth/serializers.py
@@ -3,20 +3,16 @@ from rest_framework import serializers
 
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
         )
         return user
 
