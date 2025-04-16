dd03dc61ee3dc21360b460c8809449960848fda4
hussein-miswag <hussien.hasan@miswag.com>
2025-04-16 13:34:22 +0300

    add describe image endpoint and agent

--- Domains/Results/urls.py
+++ Domains/Results/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-from Domains.Results.Views import FeynmanAgentAPIView, ZahraAgent, DavinciAgentAPIView, EinsteinAgentAPIView, HusseinAgentAPIView, FeynmanAgentAPIView, JobsAgentAPIView, UltraAgentAPIView
+from Domains.Results.Views import ImageCaptionAPIView
 
 urlpatterns = [
-    path('Jobs/', JobsAgentAPIView.as_view(), name="get_user_uploads_summary"),
-    path('feynman/', FeynmanAgentAPIView.as_view(), name="get_web_metrics"), 
-    path('Zahra/', ZahraAgent.as_view(), name="get_web_metrics"),
-    path('davinci/', DavinciAgentAPIView.as_view(), name="davinci-agent"),
-    path('einstein/', EinsteinAgentAPIView.as_view(), name="einstein-agent"),
-    path('hussein/', HusseinAgentAPIView.as_view(), name="hussein-agent"),
-    path('ultra/', UltraAgentAPIView.as_view(), name="ultra-agent")
+    path('describe-image/', ImageCaptionAPIView.as_view(), name='describe-image'),
 ]
