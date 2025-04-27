e3d180b96c26f1b675c56083ab368cc3fea61c2e
hussein-miswag <hussien.hasan@miswag.com>
2025-04-27 18:17:07 +0300

    fix alfariza

--- Domains/Results/urls.py
+++ Domains/Results/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('describe-page/', PageUIReportAPIView.as_view(), name='describe_page'),
     path('evaluate-ui/',        EvaluateUIAPIView.as_view(),   name='evaluate-ui'),
     path('evaluate-uba/', EvaluateUBAAPIView.as_view(), name='evaluate-uba'),
-    path('generate-chart/', GenerateChartsAPIView.as_view(), name='generate-chart')
+    path('generate-chart/', GenerateChartsAPIView.as_view(), name='generate-chart'),
     path('formulate-ui/',      FormulateUIAPIView.as_view(),  name='formulate-ui'),
 ]
