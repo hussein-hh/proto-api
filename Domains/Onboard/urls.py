8fe020b9263cdd86268f461d45e433488f9a9a04
ZahraFalih <zahralduraye@gmail.com>
2025-05-06 15:48:13 +0300

    delete pages

--- Domains/Onboard/urls.py
+++ Domains/Onboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserOnboardingAPIView, BusinessOnboardingAPIView, PageOnboardingAPIView, ScreenshotUploadAPIView
+from .views import UserOnboardingAPIView, BusinessOnboardingAPIView, PageOnboardingAPIView, ScreenshotUploadAPIView, PageDeleteAPIView
 
 urlpatterns = [
     path('user-onboard/', UserOnboardingAPIView.as_view(), name='user-onboard'),
     path('business-onboard/', BusinessOnboardingAPIView.as_view(), name='business-onboard'),
     path('page-onboard/', PageOnboardingAPIView.as_view(), name='page-onboard'),
     path("upload-screenshot/", ScreenshotUploadAPIView.as_view(), name="upload-screenshot"),
+    path('pages/<int:page_id>/<str:page_type>/',  PageDeleteAPIView.as_view(),   name='page-delete'),
 ]
