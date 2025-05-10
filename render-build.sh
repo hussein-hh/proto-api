2aedccb54b7565e4a47b84a802fc1a16787238e1
Hussein Hasan <hussien.hasan@miswag.com>
2025-05-10 14:47:28 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-
-# Run migrations
 python manage.py migrate --noinput
 
+# Create admin user if missing
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'AdminPass123')
+" | python manage.py shell
