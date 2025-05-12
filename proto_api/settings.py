63624b5933fb1017b52b7b4c78095e54f4aeb797
hussein-miswag <hussien.hasan@miswag.com>
2025-05-12 13:32:50 +0300

    update settings

--- proto_api/settings.py
+++ proto_api/settings.py
@@ -71,8 +71,31 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://proto-ux.netlify.app",
+    "http://localhost:3000",  # For local development
+]
+CORS_ALLOWED_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 import os
 from dotenv import load_dotenv
